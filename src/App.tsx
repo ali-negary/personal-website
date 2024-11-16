0258677aaf1db6e6499187f71fdd4b0f54b6e1ae
Ali Negary <ali.negary@outlook.com>
2024-11-16 21:33:32 +0100

    add-app & routes & pages -

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import AppRoutes from "./routes";
 import GlobalStyle from "./styles/global";
 
-const App: React.FC = () => {
-  return <AppRoutes />;
-};
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <AppRoutes />
+  </>
+);
 
 export default App;
